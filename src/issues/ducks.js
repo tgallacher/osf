30974198a78bfaaaa75e6c6491b61cbb65b611d3
Tom Gallacher <thomas.gallacher@gmail.com>
2018-01-13 22:15:39 +0000

    Add initial Issues ducks with tests

---
+++ src/issues/ducks.js
@@ -0,0 +1,56 @@
+// @flow
+import { REPOS_GET_STARRED_REQUEST } from 'repos/ducks';
+
+export type State = {
+    [full_name: string]: Object[]
+};
+
+export const defaultState: State = {};
+
+//
+// ACTION CONSTANTS
+//
+
+export const ISSUES_GET_STARRED_REQUEST = 'ISSUES.GET.STARRED.REQUEST';
+export const ISSUES_GET_STARRED_COMPLETE = 'ISSUES.GET.STARRED.COMPLETE';
+
+//
+// ACTION CREATORS
+//
+
+export const getRepoIssues = (): FSAModel => ({
+    type: ISSUES_GET_STARRED_REQUEST
+});
+
+export const getRepoIssuesComplete = (repoName: string, data: Array<Object> | Error, error?: boolean = false): FSAModel => ({
+    type: ISSUES_GET_STARRED_COMPLETE,
+    payload: {
+        data,
+        repoName
+    },
+    error
+});
+
+//
+// REDUCER
+//
+
+export default (prevState: State = defaultState, action: FSAModel): State => {
+    const { payload: { data = [], repoName } = {}, error } = action;
+
+    if(error){
+        return defaultState;
+    }
+
+    switch(action.type){
+        case REPOS_GET_STARRED_REQUEST:
+            return defaultState;
+        case ISSUES_GET_STARRED_COMPLETE:
+            return {
+                ...prevState,
+                [repoName]: repoName in prevState ? prevState[repoName].concat(data) : data
+            }
+    }
+
+    return prevState;
+}
---
+++ src/issues/specs/reducer.spec.js
@@ -0,0 +1,82 @@
+import { assert } from 'chai';
+
+import reducer, {
+    defaultState,
+    getRepoIssuesComplete
+} from '../ducks';
+import { getStarredRepos } from 'repos/ducks';
+
+describe('repos.reducer', () => {
+    it('returns the correct initial state', () => {
+        const state = reducer(undefined, {type: '@@REDUX/INIT'});
+
+        assert.isObject(state);
+        assert.deepEqual(state, defaultState, 'State should be initialised to match the default state');
+    });
+
+    it('stores a return repository object correctly', () => {
+        const repoName = 'foo/bar';
+        const exIssueAPIResp = [{id: 1, url: 'http://foo'}];
+        const state = reducer(defaultState, getRepoIssuesComplete(repoName, exIssueAPIResp));
+
+        assert.isObject(state);
+        assert.isArray(Object.keys(state));
+        assert.lengthOf(Object.keys(state), 1);
+        assert.strictEqual(Object.keys(state)[0], repoName, 'The keyed id should match');
+        assert.deepEqual(state[repoName][0], exIssueAPIResp[0], 'The stored repo object should match the returned data');
+    });
+
+    it('adds an addtional repo object to state correctly for the same repo request', () => {
+        const repoName = 'foo/bar';
+        const exIssueAPIResp = [{id: 1, url: 'http://foo'}];
+        let state = reducer(defaultState, getRepoIssuesComplete(repoName, exIssueAPIResp));
+
+        const exIssueAPIResp2 = [{id: 2, url: 'http://bar'}];
+        state = reducer(state, getRepoIssuesComplete(repoName, exIssueAPIResp2));
+
+        assert.isObject(state);
+        assert.isArray(Object.keys(state));
+        assert.lengthOf(Object.keys(state), 1);
+        assert.strictEqual(Object.keys(state)[0], repoName, 'The keyed id should match');
+        assert.deepEqual(state[repoName][0], exIssueAPIResp[0], 'The stored repo object should match the returned data');
+        assert.deepEqual(state[repoName][1], exIssueAPIResp2[0], 'The stored repo object should match the returned data');
+    });
+
+    it('stores issues for more than one repo', () => {
+        const repoName = 'foo/bar';
+        const exIssueAPIResp = [{id: 1, url: 'http://foo'}];
+        let state = reducer(defaultState, getRepoIssuesComplete(repoName, exIssueAPIResp));
+
+        const repoName2 = 'bar/baz';
+        const exIssueAPIResp2 = [{id: 2, url: 'http://bar'}];
+        state = reducer(state, getRepoIssuesComplete(repoName2, exIssueAPIResp2));
+
+        assert.isObject(state);
+        assert.isArray(Object.keys(state));
+        assert.lengthOf(Object.keys(state), 2);
+        assert.strictEqual(Object.keys(state)[0], repoName, 'The keyed id should match');
+        assert.strictEqual(Object.keys(state)[1], repoName2, 'The keyed id should match');
+        assert.deepEqual(state[repoName][0], exIssueAPIResp[0], 'The stored repo object should match the returned data');
+        assert.deepEqual(state[repoName2][0], exIssueAPIResp2[0], 'The stored repo object should match the returned data');
+    });
+
+    it('resets the state when a request results in an error', () => {
+        const repoName = 'foo/bar';
+        const state = reducer(defaultState, getRepoIssuesComplete(repoName, new Error('Some API error'), true));
+
+        assert.isObject(state);
+        assert.deepEqual(state, defaultState, 'State should be initialised to match the default state');
+    });
+
+    it('resets the state when a new get starred repos request is made', () => {
+        const repoName = 'foo/bar';
+        const exIssueAPIResp = [{id: 1, url: 'http://foo'}];
+        let state = reducer(defaultState, getRepoIssuesComplete(repoName, exIssueAPIResp));
+
+        assert.isObject(state);
+        assert.lengthOf(Object.keys(state), 1);
+
+        state = reducer(state, getStarredRepos());
+        assert.deepEqual(state, defaultState, 'State should be initialised to match the default state');
+    });
+});
