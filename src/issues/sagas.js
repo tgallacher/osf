8a7779b6c1aa153331f78ec9ac53beb9c4002c8d
Tom Gallacher <thomas.gallacher@gmail.com>
2018-01-13 23:52:04 +0000

    Add basic issues saga

---
+++ src/issues/sagas.js
@@ -0,0 +1,35 @@
+/* global __PRODUCTION__ */
+import { takeEvery, call, put } from 'redux-saga/effects';
+
+import {
+    getRepoIssuesComplete,
+    ISSUES_GET_STARRED_REQUEST
+} from './ducks';
+import { get } from 'utils/http';
+
+/**
+ * Fetch all open issues for a given repo. This is
+ * currently filtered to only display issues with a 'help wanted' label.
+ *
+ * @param {FSAModel} action - Redux action object
+ */
+export const getRepoIssuesSaga = function* (action){
+    const { payload: { repoName } = {} } = action;
+
+    try {
+        if( ! repoName)
+            return;
+
+        const repoIssues = yield call(get, `https://api.github.com/repos/${repoName}/issues?labels=help+wanted`);
+
+        yield put(getRepoIssuesComplete(repoName, repoIssues));
+    }
+    catch (error){
+        yield put(getRepoIssuesComplete(repoName, error, true));
+        if( ! __PRODUCTION__){ throw error; }
+    }
+}
+
+export default function* (){
+    yield takeEvery(ISSUES_GET_STARRED_REQUEST, getRepoIssuesSaga);
+}
