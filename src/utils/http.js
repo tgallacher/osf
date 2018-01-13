e7f1c0864d1ebb5eefec039e441802ff32be18c8
Tom Gallacher <thomas.gallacher@gmail.com>
2018-01-13 23:48:06 +0000

    Add http utils + fix outdated package deps

---
+++ src/utils/http.js
@@ -0,0 +1,48 @@
+// @flow
+import fetch from 'isomorphic-fetch';
+
+const defHeaders = {
+    'Content-Type': 'application/json'
+}
+
+/**
+ * Simple helper to check response and throw useful error
+ * if the Http request did not succeed with 200-300 response.
+ *
+ * @param {Response} resp - HTTP response object
+ * @returns {Object} Http response body
+ * @throws {RDSError} If RDS returns an error
+ */
+const checkForHttpErrorsAndThrow = async (resp: Object): Promise<any> => {
+    const respData = await resp.json();
+
+    if( resp.status >= 400 ){
+        const msg = respData.message || resp.statusText; // Github will put an error message in the returned payload
+
+        throw new Error(`${resp.status} Error: ${msg}`);
+    }
+
+    return respData;
+}
+
+/**
+ * Perform a GET request
+ *
+ * @param  {string} endpoint - RDS URI endpoint
+ * @param  {Object} headers -  Optional HTTP headers to send with request
+ * @returns {Promise} ES6 thenable
+ * @throws {RDSError} If RDS returns an error
+ * @since 1.0.0
+ * @example
+ * let data = requests.get('/coupons/coupon/XXXX-XXXX-XXXX-XXXX').then( resp => resp );
+ */
+export const get = async (url: string, headers: Object = {}): Promise<any> => {
+    const options = {
+        method: 'GET',
+        headers: { ...defHeaders, ...headers }
+    };
+
+    const resp = await fetch(url, options);
+
+    return await checkForHttpErrorsAndThrow(resp);
+}
